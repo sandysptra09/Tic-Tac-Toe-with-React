59f2b7eeb942f7ef0addbf23f41a86796e3fbdc7
Sandy Saputra <21221753145@smkassalaambandung.sch.id>
2023-10-26 14:42:33 +0700

    Create user input with two-way-binding

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
+import Player from "./components/Player";
+
 function App() {
   return (
     <main>
       <div id="game-container">
         <ol id="players">
-          <li>
-            <span className="player-name">Player 1</span>
-            <span className="player-symbol">X</span>
-          </li>
-          <li>
-            <span className="player-name">Player 2</span>
-            <span className="player-symbol">O</span>
-          </li>
+          <Player initialName="Player 1" symbol="X" />
+          <Player initialName="Player 2" symbol="O" />
         </ol>
         GAME BOARD
       </div>

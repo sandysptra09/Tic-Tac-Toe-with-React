e58575a937ff1cd87909df82caac6273865669fa
Sandy Saputra <21221753145@smkassalaambandung.sch.id>
2023-10-27 10:13:53 +0700

    Create log with template literals

--- src/App.jsx
+++ src/App.jsx
@@ -43,7 +43,7 @@ function App() {
         </ol>
         <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
       </div>
-      <Log />
+      <Log turns={gameTurns} />
     </main>
   );
 }
